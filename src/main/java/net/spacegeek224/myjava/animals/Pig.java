db56c4d93ac48f0d8d92bf7e70da688e6f44580b
spacegeek224 <marisusisme@gmail.com>
2016-02-12 19:26:46 +0000

    d

---
+++ src/main/java/net/spacegeek224/myjava/animals/Pig.java
@@ -0,0 +1,18 @@
+package net.spacegeek224.myjava.animals;
+
+import net.spacegeek224.myjava;
+
+
+/**
+ * The Pig Class Extending The Animal Class
+ * 
+ */
+public class Pig extends Animal {
+    /**
+     * Makes the Pig Oink.
+     * 
+     */
+    public void oink() {
+        System.out.println("Oink!");
+    }
+}
